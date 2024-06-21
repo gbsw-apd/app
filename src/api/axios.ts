5eec3857d2872be2f6d03fcffe917ea50d6a132b
wlstmd <jinwoo8288@gmail.com>
2024-06-22 02:01:59 +0900

    feat: axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: 'http://localhost:3030',
+  // baseURL: 'http://localhost:3030',
+  baseURL: 'http://43.202.37.122:3030',
   withCredentials: true,
 });
 
